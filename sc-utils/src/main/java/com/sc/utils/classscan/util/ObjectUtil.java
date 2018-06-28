905569c7f4af3a57c1484d90b999ce5a38e786c8
qisensen <senssic@foxmail.com>
2018-06-28 10:05:46 +0800

    增加类扫描相关工具类

---
+++ sc-utils/src/main/java/com/sc/utils/classscan/util/ObjectUtil.java
@@ -0,0 +1,50 @@
+package com.sc.utils.classscan.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/**
+ * 对象操作工具类
+ */
+public class ObjectUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(ObjectUtil.class);
+
+    /**
+     * 复制所有成员变量
+     */
+    public static void copyFields(Object source, Object target) {
+        try {
+            for (Field field : source.getClass().getDeclaredFields()) {
+                // 若不为 static 成员变量，则进行复制操作
+                if (!Modifier.isStatic(field.getModifiers())) {
+                    field.setAccessible(true); // 可操作私有成员变量
+                    field.set(target, field.get(source));
+                }
+            }
+        } catch (Exception e) {
+            logger.error("复制成员变量出错！", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 通过反射创建实例
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T newInstance(String className) {
+        T instance;
+        try {
+            Class<?> commandClass = ClassUtil.loadClass(className);
+            instance = (T) commandClass.newInstance();
+        } catch (Exception e) {
+            logger.error("创建实例出错！", e);
+            throw new RuntimeException(e);
+        }
+        return instance;
+    }
+
+}

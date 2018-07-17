84f9b80dc31b7ec94622eea8637037f7926b6a3d
qisensen <senssic@foxmail.com>
2018-07-17 17:33:30 +0800

    redis简单队列

---
+++ sc-redis/src/main/java/com/sc/redis/listener/SmsMessageDelegateListener.java
@@ -0,0 +1,18 @@
+package com.sc.redis.listener;
+
+
+import java.io.Serializable;
+
+/**
+ * redis 简单队列
+ * 1.必须有handleMessage
+ * 2.方法中必须是Serializable的实现类
+ */
+public class SmsMessageDelegateListener {
+    //监听Redis消息必须有handleMessage 且获取到的必须是实现Serializable
+    public void handleMessage(Serializable message) {
+
+        System.out.println("-->监听到数据" + message);
+
+    }
+}

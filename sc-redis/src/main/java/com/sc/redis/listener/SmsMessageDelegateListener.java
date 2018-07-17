f6f0edccf8043102c2a7c659d97dce9916c48a35
qisensen <senssic@foxmail.com>
2018-07-17 17:36:51 +0800

    redis简单队列

--- sc-redis/src/main/java/com/sc/redis/listener/SmsMessageDelegateListener.java
+++ sc-redis/src/main/java/com/sc/redis/listener/SmsMessageDelegateListener.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
  * redis 简单队列
  * 1.必须有handleMessage
  * 2.方法中必须是Serializable的实现类
+ * 3.发送端使用redisTemplate.convertAndSend("sms_queue_web_online", smsMessageVo);进行广播消息
  */
 public class SmsMessageDelegateListener {
     //监听Redis消息必须有handleMessage 且获取到的必须是实现Serializable
